48a51a5b40942c4da8120d568959804a77e894db
Iren25 <irenfrank.de@gmail.com>
2023-09-21 20:56:21 +0200

    git add . git commit -m 'added new changes'

--- src/main/java/DemoWebShop/utils/DataProviders.java
+++ src/main/java/DemoWebShop/utils/DataProviders.java
@@ -19,9 +19,9 @@ public class DataProviders {
     @DataProvider
     public Iterator<Object[]> newUser() {
         List<Object[]> list = new ArrayList<>();
-        list.add(new Object[] {"Ronald1", "Smith", "smith017@gmail.com", "qwerty001", "qwerty001"});
-        list.add(new Object[] {"Ronald2", "Smith", "smith018@gmail.com", "qwerty002", "qwerty002"});
-        list.add(new Object[] {"Ronald3", "Smith", "smith019@gmail.com", "qwerty003", "qwerty003"});
+        list.add(new Object[] {"Ronald1", "Smith", "smith020@gmail.com", "qwerty001", "qwerty001"});
+        list.add(new Object[] {"Ronald2", "Smith", "smith021@gmail.com", "qwerty002", "qwerty002"});
+        list.add(new Object[] {"Ronald3", "Smith", "smith022@gmail.com", "qwerty003", "qwerty003"});
 
         return list.iterator();
     }
